2bacf11c2db5bb9b48277eb8ed73ea2ff54dc340
Andres <marianoandres191200@hotmail.com>
2025-05-21 22:16:03 -0300

    added base variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import styles from './styles.module.scss';
+
 export default function Home() {
   return (
-    <main>
+    <main className={styles.homeMainContainer}>
       <h1>SPOTIFY DESKTOP HOME with tanstack</h1>
     </main>
   );
