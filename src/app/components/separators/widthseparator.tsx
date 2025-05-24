da65d6f780ab6f116fcc724899d73c6f7c9759c8
Andres <marianoandres191200@hotmail.com>
2025-05-24 14:49:10 -0300

    added some links

---
+++ src/app/components/separators/widthseparator.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const spacings = {
+    mini: 'var(--padding-mini)',
+  small: "var(--padding-sm)",
+  medium: "var(--padding-md)",
+  large: "var(--padding-lg)",
+};
+
+type TSpacing = keyof typeof spacings;
+
+export default function WidthSeparator({
+  spacing,
+}: {
+  spacing: TSpacing;
+}) {
+  return <div style={{ paddingInline: spacings[spacing] }}></div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from './components/navbar';
+import Navbar from './components/navbar/navbar';
 import styles from './styles.module.scss';
 
 export default function Home() {
