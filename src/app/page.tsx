2b59f1ca5a7e3a509ba3f4d62f840be9169b849f
Andres <marianoandres191200@hotmail.com>
2025-05-24 02:08:39 -0300

    added some icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Navbar from './components/navbar';
 import styles from './styles.module.scss';
 
 export default function Home() {
   return (
     <main className={styles.homeMainContainer}>
+      <Navbar />
       <h1>SPOTIFY DESKTOP HOME with tanstack</h1>
     </main>
   );
