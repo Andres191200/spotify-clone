7880604cfab55fa109164ad901cccacfcff92c2e
Andres <marianoandres191200@hotmail.com>
2025-06-10 00:28:06 -0300

    created rowtile model and components

---
+++ src/app/components/row-tile/RowTileElement.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Tile } from './tile'
+import styles from './styles.module.scss';
+
+export default function RowTileElement(element: Tile) {
+  return (
+    <div className={styles.rowTileElementComponent}>
+        <div className={styles.thumbnail}></div>
+        <h5>{element.title}</h5>
+        <h5>{element.subtitle}</h5>
+
+    </div>
+  )
+}
