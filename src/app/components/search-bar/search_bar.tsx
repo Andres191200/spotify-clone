e5083030bdce16a18183402f051d6de87b0daf16
Andres <marianoandres191200@hotmail.com>
2025-06-26 22:10:54 -0300

    created subfolders for artist and albums, and added search icon to the search bar component

--- src/app/components/search-bar/search_bar.tsx
+++ src/app/components/search-bar/search_bar.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import styles from './styles.module.scss';
+import Image from 'next/image';
 
 export default function SearchBar() {
   return (
     <div className={styles.searchBarComponent}>
         <input type="text" placeholder='What do you want to play?'/>
+        <div className={styles.iconContainer}>
+          <Image src={'/zoom_icon.svg'} height={30} width={30} alt="search icon"/>
+        </div>
     </div>
   )
 }
